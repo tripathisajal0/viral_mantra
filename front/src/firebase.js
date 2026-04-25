b4e1abdfd5e864720d5cad451094a18bf219dc5f
Sanketha_Kumbar <sankethkumbar@gmail.com>
2026-04-25 19:24:58 +0530

    Move Firebase secrets to environment variables

--- front/src/firebase.js
+++ front/src/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 
 // REPLACE THESE with your actual config from Firebase Console!
 const firebaseConfig = {
-  apiKey: "AIzaSyCbuti144CurPSqYNmP-3Dn7jeHMRwWU3Y",
-  authDomain: "viralmantra.firebaseapp.com",
-  projectId: "viralmantra",
-  storageBucket: "viralmantra.firebasestorage.app",
-  messagingSenderId: "476543900015",
-  appId: "1:476543900015:web:1c741819e53ae24293b09a",
-  measurementId: "G-T4W42E6N79"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
